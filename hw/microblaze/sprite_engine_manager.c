dba2c291490d6af04bf455e1604e966a6ab832ec
Scott Hendrickson <shendrickso2@wisc.edu>
2015-12-16 10:05:44 -0600

    resize BRAM to fit for bg

--- hw/microblaze/sprite_engine_manager.c
+++ hw/microblaze/sprite_engine_manager.c
@@ -39,7 +39,7 @@
 
 #include "hw/stream.h"
 
-#define LMB_BRAM_SIZE  (128 * 1024)
+#define LMB_BRAM_SIZE  (4 * 128 * 1024)
 #define FLASH_SIZE     (32 * 1024 * 1024)
 
 #define BINARY_DEVICE_TREE_FILE "petalogix-ml605.dtb"
